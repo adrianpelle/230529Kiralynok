2de11633c215fe77de443bdaa0e273162ef268cb
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-06-02 21:33:13 +0200

    success task 7

--- 0529Sakk/src/sakk/SakkMain.java
+++ 0529Sakk/src/sakk/SakkMain.java
@@ -7,6 +7,7 @@ public class SakkMain {
 
 		Tabla t = new Tabla();
 		ClassForFifthTask.Elhelyez(8, t);
+		System.out.println("Feladat 7 (uresSor(0) ) : "+ClassForSeventhTask.UresOszlop(0, t));
 		NegyedikFeladatMegjelenit.Megjelenit(t);
 		
 		
